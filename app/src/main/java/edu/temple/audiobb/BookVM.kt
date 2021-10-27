db3a60f014b9b3f5d392ed77e1e622f9361fa9a6
yasminewatkins <yasminepwatkins@gmail.com>
2021-10-27 15:38:02 -0400

    Made updates to Book View Model and recycler view

---
+++ app/src/main/java/edu/temple/audiobb/BookVM.kt
@@ -0,0 +1,25 @@
+package edu.temple.audiobb
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookVM: ViewModel() {
+    private val book : MutableLiveData<Book> by lazy {
+        MutableLiveData<Book>()
+    }
+    private var empty: Boolean = true
+
+    fun getBook() : LiveData<Book> {
+        return book
+    }
+
+    fun setBook(book: Book) {
+        this.book.value = book
+        empty = false
+    }
+
+    fun isEmpty() : Boolean {
+        return empty
+    }
+}
