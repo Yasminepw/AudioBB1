171026dc0a6ca335a3199dfb06b4e2924783bac7
yasminewatkins <yasminepwatkins@gmail.com>
2021-11-10 10:40:54 -0500

    Updated book class

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -1,4 +1,5 @@
 package edu.temple.audiobb
 
-data class Book(val title: String, val author: String) {
-}
+import java.io.Serializable
+
+data class Book(val title: String, val author: String) : Serializable
