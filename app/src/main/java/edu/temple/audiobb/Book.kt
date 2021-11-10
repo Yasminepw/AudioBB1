6b0ee564abe4f78faaa4eaa5e9eaec454dcc4cfd
yasminewatkins <yasminepwatkins@gmail.com>
2021-11-10 10:45:13 -0500

    Updated book class by adding id: Int and coverURL: String

--- app/src/main/java/edu/temple/audiobb/Book.kt
+++ app/src/main/java/edu/temple/audiobb/Book.kt
@@ -2,4 +2,4 @@ package edu.temple.audiobb
 
 import java.io.Serializable
 
-data class Book(val title: String, val author: String) : Serializable
+data class Book(val id: Int, val title: String, val author: String, val coverURL: String): Serializable
